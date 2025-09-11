0abdf299e39bf709fb9c7445a80e1a3a0b05e9da
KazuyaFujino1231 <tounonarimasa1231@gmail.com>
2025-09-12 03:25:34 +0900

    コメント追加

--- sample/view/views/tweet_view.py
+++ sample/view/views/tweet_view.py
@@ -5,5 +5,9 @@ from sample.serializer.tweet_serializer import TweetSerializer
 
 
 class TweetViewSet(ModelViewSet):
+    """
+    デフォルトアクションのお勉強用。
+    DRFによって自動的に提供される。車輪の再開発が起こりやすいポイントなので要注意。
+    """
     queryset = Tweet.objects.all()
     serializer_class = TweetSerializer
